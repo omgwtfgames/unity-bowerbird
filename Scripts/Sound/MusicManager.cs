2c7642fc445b8977376a2179ba19474372dfa4dc
Andrew Perry <ajperry@pansapiens.com>
2013-05-07 10:03:04 +1000

    New singleton SoundManager in C#, added camera parenting and turned doppler off.

--- Scripts/Sound/MusicManager.cs
+++ Scripts/Sound/MusicManager.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
 public class MusicManager : MonoBehaviour {
   public AudioClip[] tracks;
 	public bool playOnStart = true;
+	public bool parentToMainCamera = true;
 	public int defaultTrack = 0;
 	public float _volume = 1.0f;
 	public float _pitch = 1.0f;
 	
 	// Static singleton property
-  public static MusicController Instance { get; private set; }
+    public static MusicManager Instance { get; private set; }
  
 	public float volume {
 		get { return _volume; }
@@ -25,11 +27,9 @@ public class MusicManager : MonoBehaviour {
 		    }
 	}
 
-    void Awake()
-    {
+    void Awake() {
         // First we check if there are any other instances conflicting
-        if(Instance != null && Instance != this)
-        {
+        if(Instance != null && Instance != this) {
             // If that is the case, we destroy other instances
             Destroy(gameObject);
 			return;
@@ -40,14 +40,22 @@ public class MusicManager : MonoBehaviour {
  
         // Furthermore we make sure that we don't destroy between scenes (this is optional)
         DontDestroyOnLoad(gameObject);
-		
+			
 		audio.clip = tracks[defaultTrack];
+		
+		GetComponent<AudioSource>().rolloffMode = AudioRolloffMode.Linear;
+		GetComponent<AudioSource>().loop = true;
+		GetComponent<AudioSource>().dopplerLevel = 0f;
     }
 	
 	void Start() {
 		if (playOnStart) {
 			Play();
 		}
+	}
+	
+	void OnLevelWasLoaded (int level) {
+		if (parentToMainCamera) transform.parent = Camera.main.transform;
 	}
 	
 	public void PlayTrack(int i) {
