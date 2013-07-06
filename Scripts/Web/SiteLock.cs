98a8a307e89d13aadbee3f31b3fa5f9d946601b3
Andrew Perry <ajperry@pansapiens.com>
2013-07-06 16:45:59 +1000

    SiteLock now allows multiple whitelisted domains.

--- Scripts/Web/SiteLock.cs
+++ Scripts/Web/SiteLock.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SiteLock : MonoBehaviour {
-	public string domain;
+	public List<string> domains;
 	public string redirectUrl;
 	
 	void Awake () {
-		Application.ExternalEval("if(document.location.host != '"+domain+
-			                     "') { document.location='"+redirectUrl+
-			                     "'; }");
-	}
-}	
+        #if UNITY_WEBPLAYER
+		string jsarray = "[";
+		foreach (string domain in domains) {
+			jsarray += "'"+domain+"',";
+		}
+		jsarray += "]";
+		Application.ExternalEval("function contains(a, obj) { " +
+			                       "var i = a.length; " +
+			                       "while (i--) { if (a[i] === obj) { return true;}} " +
+			                       "return false;" +
+			                      "} "+
+			                     "if(contains("+jsarray+", document.location.host)) {} else { document.location='"+redirectUrl+"'; }");
+        #endif
+    }
+}
