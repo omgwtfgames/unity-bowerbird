a13e03e3e128ebf291535d025ee4b18dfb7750bf
Andrew Perry <perry@omgwtfgames.com>
2013-08-02 12:30:25 +1000

    Changed screenshot filenaming convention, fixed infinite loop when screenshoting while paused.

--- Scripts/Editor/TakeScreenshotInEditor.cs
+++ Scripts/Editor/TakeScreenshotInEditor.cs
@@ -3,24 +3,30 @@
 
 using UnityEngine;
 using UnityEditor;
- 
+
 public class TakeScreenshotInEditor : ScriptableObject
 {
-    public static string fileName = "Editor Screenshot ";
+    public static string suffix = "Editor Screenshot ";
     public static int startNumber = 1;
  
     [MenuItem ("Custom/Take Screenshot of Game View %^s")]
     static void TakeScreenshot()
     {
         int number = startNumber;
-        string name = "" + number;
-        string filepath = System.IO.Path.Combine(Application.dataPath, fileName + name + ".png");
+        string date = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:FFF ");
+		string filepath = System.IO.Path.Combine(Application.dataPath, date + suffix + number.ToString() + ".png");
 		
+		    /*
+        // disabled file existence check and added timestamp instead,
+        // since this seems to get into an infinite loop
+        // if called when the editor is paused.
         while (System.IO.File.Exists(filepath))
         {
             number++;
             name = "" + number;
+			      if (number > 999) break;
         }
+       */
  
         startNumber = number + 1;
  
