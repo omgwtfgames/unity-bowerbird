d810dd9bd370c7f4f60cd76cd719479c39f36dce
Andrew Perry <perry@omgwtfgames.com>
2014-08-18 13:18:56 +1000

    Editor script to force audio to import as 2D sound.

---
+++ Scripts/Editor/Force2DSound.cs
@@ -0,0 +1,20 @@
+﻿// Snippet from: http://fortressfiasco.wordpress.com/2013/06/03/how-to-force-all-new-sounds-to-be-2d-in-unity/
+// Forces all imported sounds to be set as 2D sounds.
+
+using UnityEditor;
+using UnityEngine;
+
+public class Force2DSound : AssetPostprocessor {	
+
+	// Make this true to force 2D sound importing.
+	// (initially set to false by default as a precaution, since it overrides expected Unity behaviour)
+	bool forceSoundImportTo2D = false;
+
+	public void OnPreprocessAudio() {
+		if (forceSoundImportTo2D) {
+			AudioImporter ai = assetImporter as AudioImporter;
+			ai.threeD = false;
+			Debug.Log("Forcing import of sound as 2D: " + ai.assetPath);
+		}
+	}
+}
