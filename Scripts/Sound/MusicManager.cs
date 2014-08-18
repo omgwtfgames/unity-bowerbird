b384bc83d240ce2bfe6fc70a041a74f370ca6f46
Andrew Perry <perry@omgwtfgames.com>
2014-08-18 13:27:26 +1000

    Scaled output volume for music and sound.

--- Scripts/Sound/MusicManager.cs
+++ Scripts/Sound/MusicManager.cs
@@ -3,38 +3,50 @@ using System.Collections;
 
 [RequireComponent (typeof(AudioSource))]
 public class MusicManager : MonoBehaviour {
-  public AudioClip[] tracks;
-	public bool playOnStart = true;
-	public bool parentToMainCamera = true;
-	public int defaultTrack = 0;
-	public string preferenceName = "music_volume";
-	public bool initialVolumeFromPreference = true;
-	public float _volume = 1.0f;
-	public float _pitch = 1.0f;
-	
-	// Static singleton property
+    public AudioClip[] tracks;
+    public bool playOnStart = true;
+    public enum CameraFollowMode {
+        None,
+        Follow,
+        Child
+    };
+    public CameraFollowMode cameraFollowMode = CameraFollowMode.Follow;
+    public int defaultTrack = 0;
+    public string preferenceName = "music_volume";
+    public bool initialVolumeFromPreference = true;
+    public float _volume = 1.0f;
+    public float _pitch = 1.0f;
+	public bool ScaleOutputVolume = true;
+
+    // Static singleton property
     public static MusicManager Instance { get; private set; }
  
-	public float volume {
-		get { return _volume; }
-		set { audio.volume = value;
-			  _volume = value;
-		    }
-	}
-	
-	public float pitch {
-		get { return _pitch; }
-		set { audio.pitch = value;
-			  _pitch = value;
-		    }
-	}
-	
+    public float volume {
+        get { return _volume; }
+        set {
+            if (ScaleOutputVolume) {
+				audio.volume = ScaleVolume(value);
+			} else {
+				audio.volume = value;
+			}
+            _volume = value;
+        }
+    }
+ 
+    public float pitch {
+        get { return _pitch; }
+        set {
+            audio.pitch = value;
+            _pitch = value;
+        }
+    }
+ 
     void Awake() {
         // First we check if there are any other instances conflicting
-        if(Instance != null && Instance != this) {
+        if (Instance != null && Instance != this) {
             // If that is the case, we destroy other instances
             Destroy(gameObject);
-			return;
+            return;
         }
  
         // Here we save our singleton instance
@@ -42,85 +54,115 @@ public class MusicManager : MonoBehaviour {
  
         // Furthermore we make sure that we don't destroy between scenes (this is optional)
         DontDestroyOnLoad(gameObject);
-		
-		  if (parentToMainCamera) transform.parent = Camera.main.transform;
-		
-		  audio.clip = tracks[defaultTrack];
-		  volume = _volume;
-		  if (initialVolumeFromPreference) volume = GetVolumePreference();
-		
-		  AudioSource audioSrc = GetComponent<AudioSource>();
-		  audioSrc.rolloffMode = AudioRolloffMode.Linear;
-		  audioSrc.loop = true;
-		  audioSrc.dopplerLevel = 0f;
-	    audioSrc.panLevel = 0f;
+     
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        } else if (cameraFollowMode == CameraFollowMode.Child) {
+            if (Camera.main != null)
+                transform.parent = Camera.main.transform;
+        } 
+     
+        audio.clip = tracks[defaultTrack];
+        volume = _volume;
+        if (initialVolumeFromPreference)
+            volume = GetVolumePreference();
+     
+        AudioSource audioSrc = GetComponent<AudioSource>();
+        audioSrc.rolloffMode = AudioRolloffMode.Linear;
+        audioSrc.loop = true;
+        audioSrc.dopplerLevel = 0f;
+        audioSrc.panLevel = 0f;
     }
-	
-	void Start() {
-		if (playOnStart) {
-			Play();
-		}
-	}
-	
-	void OnLevelWasLoaded (int level) {
-		if (parentToMainCamera) transform.parent = Camera.main.transform;
-	}
-	
-	public void PlayTrack(int i) {
-	  audio.Stop();
-	  audio.clip = tracks[i];
-	  audio.Play();
-	}
-	
-	public void Pause() {
-		audio.Pause();
-	}
-	
-	public void Play() {
-		audio.Play();
-	}
-	
-	public void Stop() {
-		audio.Stop();
-	}
-	
-	public void Fade(float targetVolume, float fadeTime) {
-		LeanTween.value(gameObject, "SetVolume", volume, targetVolume, fadeTime);
-	}
-	
-	public void FadeOut(float fadeTime) {
-		StartCoroutine(FadeOutAsync(fadeTime));
-	}
-	
-	IEnumerator FadeOutAsync(float fadeTime) {
-		Fade(0f, fadeTime);
-		yield return new WaitForSeconds(fadeTime);
-		Pause();
-	}
-	
-	public void FadeIn(float fadeTime) {
-		Play();
-		Fade(1.0f, fadeTime);
-	}
-	
-	public void SlidePitch(float targetPitch, float fadeTime) {
-		LeanTween.value(gameObject, "SetPitch", pitch, targetPitch, fadeTime);
-	}
-	
-	public float GetVolumePreference() {
-		return PlayerPrefs.GetFloat(preferenceName, volume);
-	}
-	
-	public void SaveCurrentVolumePreference() {
-		SaveVolumePreference(volume);
-	}
-	
-	public void SaveVolumePreference(float v) {
-		PlayerPrefs.SetFloat(preferenceName, v);
-		PlayerPrefs.Save();
-	}
-	
-	public void SetPitch(float p) {
-		pitch = p;
+ 
+    void Start() {
+        if (playOnStart) {
+            Play();
+        }
+    }
+ 
+    void OnLevelWasLoaded(int level) {
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        } else if (cameraFollowMode == CameraFollowMode.Child) {
+            if (Camera.main != null)
+                transform.parent = Camera.main.transform;
+        } 
+    }
+ 
+    void Update() {
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        }
+    }
+ 
+    public void PlayTrack(int i) {
+        audio.Stop();
+        audio.clip = tracks[i];
+        audio.Play();
+    }
+ 
+    public void Pause() {
+        audio.Pause();
+    }
+ 
+    public void Play() {
+        audio.Play();
+    }
+ 
+    public void Stop() {
+        audio.Stop();
+    }
+ 
+    public void Fade(float targetVolume, float fadeTime) {
+        LeanTween.value(gameObject, "SetVolume", volume, targetVolume, fadeTime);
+    }
+ 
+    public void FadeOut(float fadeTime) {
+        StartCoroutine(FadeOutAsync(fadeTime));
+    }
+ 
+    IEnumerator FadeOutAsync(float fadeTime) {
+        Fade(0f, fadeTime);
+        yield return new WaitForSeconds(fadeTime);
+        Pause();
+    }
+ 
+    public void FadeIn(float fadeTime) {
+        Play();
+        Fade(1.0f, fadeTime);
+    }
+ 
+    public void SlidePitch(float targetPitch, float fadeTime) {
+        LeanTween.value(gameObject, "SetPitch", pitch, targetPitch, fadeTime);
+    }
+ 
+    public float GetVolumePreference() {
+        return PlayerPrefs.GetFloat(preferenceName, volume);
+    }
+ 
+    public void SaveCurrentVolumePreference() {
+        SaveVolumePreference(volume);
+    }
+ 
+    public void SaveVolumePreference(float v) {
+        PlayerPrefs.SetFloat(preferenceName, v);
+        PlayerPrefs.Save();
+    }
+ 
+    public void SetPitch(float p) {
+        pitch = p;
+    }
+
+	// TODO: we should consider using this dB scale as an option when porting these changes 
+	//       over to unity-bowerbird: http://wiki.unity3d.com/index.php?title=Loudness
+	/*
+	 *   Quadratic scaling of actual volume used by AudioSource. Approximates the proper exponential.
+	 */
+	public float ScaleVolume(float v) {
+		v = Mathf.Pow(v, 4);
+		return Mathf.Clamp(v, 0f, 1f);
 	}
 }
--- Scripts/Sound/SoundManager.cs
+++ Scripts/Sound/SoundManager.cs
@@ -2,180 +2,191 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-
 public class SoundManager : MonoBehaviour {
-	public bool soundOn = true;
-	public enum CameraFollowMode { None, Follow, Child };
-	public CameraFollowMode cameraFollowMode = CameraFollowMode.Follow;
-	public float volume = 1.0f;
-	public string preferenceName = "sfx_volume";
-	public bool initialVolumeFromPreference = true;
-	public bool useFilenameAsSoundName = false;
+    public bool soundOn = true;
+    public enum CameraFollowMode {
+        None,
+        Follow,
+        Child
+    };
+    public CameraFollowMode cameraFollowMode = CameraFollowMode.Follow;
+    public float volume = 1.0f;
+	public bool ScaleOutputVolume = true;
+    public string preferenceName = "sfx_volume";
+    public bool initialVolumeFromPreference = true;
+    public bool useFilenameAsSoundName = true;
     public List<string> soundNames;
-	public AudioClip[] sounds;
-	
-	Dictionary<string,AudioClip> soundMap = new Dictionary<string,AudioClip>();
-	
-	// we keep tally of how many sfx of a particular name are playing simultaneously
-	// and don't play more than a certain number at a time (for efficiency doesn't 
-	// account for sounds that are manually stopped, assumes they play for the full duration
-	// after being triggered)
-	public int maxSimlutSfxOfOneType = 4;
-	Dictionary<string,int> currentlyPlayingCount = new Dictionary<string,int>();
-	
-	// We essentially create a static object pool, with a set number of
-	// AudioSources ('channels'). We cycle through these each time we play
-	// a sound - this will reduce the change of one sample somehow clobbering
-	// another, and allows one channel to play at a different pitch without
-	// disrupting other playing samples.
-	public int numberOfChannels = 2;
-	private AudioSource[] channels;
-	private int channelIndex = 0;
+    public AudioClip[] sounds;
+    Dictionary<string,AudioClip> soundMap = new Dictionary<string,AudioClip>();
+ 
+    // we keep tally of how many sfx of a particular name are playing simultaneously
+    // and don't play more than a certain number at a time (for efficiency doesn't 
+    // account for sounds that are manually stopped, assumes they play for the full duration
+    // after being triggered)
+    public int maxSimlutSfxOfOneType = 4;
+    Dictionary<string,int> currentlyPlayingCount = new Dictionary<string,int>();
+ 
+    // We essentially create a static object pool, with a set number of
+    // AudioSources ('channels'). We cycle through these each time we play
+    // a sound - this will reduce the change of one sample somehow clobbering
+    // another, and allows one channel to play at a different pitch without
+    // disrupting other playing samples.
+    public int numberOfChannels = 16;
+    private AudioSource[] channels;
+    private int channelIndex = 0;
 
-	// Static singleton property
-  	public static SoundManager Instance { get; private set; }
-	
-	public void Awake () {
-	    /// singleton stuff ///////
-	    // First we check if there are any other instances conflicting
-	    if (Instance != null && Instance != this) {
-	        // If that is the case, we destroy this instance
-	        Destroy(gameObject);
-			return;
-	    }
-	
-	    // Here we save our singleton instance
-	    Instance = this;
-	
-	    // Furthermore we make sure that we don't destroy between scenes (this is optional)
-	    DontDestroyOnLoad(gameObject);
-	  /////////////////////////////
-		
-	  if (numberOfChannels == null) {
-	  	numberOfChannels = 8;
-	  }
-	  channels = new AudioSource[numberOfChannels];
-	  for (int i = 0; i < sounds.Length; i++) {
-	  	if (useFilenameAsSoundName) {
-			soundNames.Add(sounds[i].name);
-			soundMap.Add(sounds[i].name, sounds[i]);
-		} else {
-			soundMap.Add(soundNames[i], sounds[i]);
-		}
-		currentlyPlayingCount.Add(soundNames[i], 0);
-	  }
-	  /*
-	  soundMap = {"Die": sounds[0],
-	            "BeamIn" : sounds[1],
-	            "PowerupPickup": sounds[2] 
-	            };
-	  */
-	  		
-	  // generate attached gameobjects "Channel1", "Channel2" with AudioSources
-	  // accessible in the channel array
-	  for (int ii = 0; ii < numberOfChannels; ii++) {
-	    	GameObject c = new GameObject();
-	    	c.name = "Channel"+ii;
-	    	c.transform.parent = transform;
-	    	AudioSource a = c.AddComponent<AudioSource>();
-	    	a.rolloffMode = AudioRolloffMode.Linear;
-		  a.panLevel = 0f;
-		  a.dopplerLevel = 0f;
-	    	c.audio.loop = false;
-	    	channels[ii] = c.audio;
-	  }
-			
-	  DontDestroyOnLoad(transform.gameObject);
-	
-		if (cameraFollowMode == CameraFollowMode.Follow) {
-			transform.position = Camera.main.transform.position;
-		}
-		else if (cameraFollowMode == CameraFollowMode.Child) {
-			transform.parent = Camera.main.transform;
-		} 
-		
-	  if (initialVolumeFromPreference) volume = GetVolumePreference();
+    // Static singleton property
+    public static SoundManager Instance { get; private set; }
+ 
+    public void Awake() {
+        /// singleton stuff ///////
+        // First we check if there are any other instances conflicting
+        if (Instance != null && Instance != this) {
+            // If that is the case, we destroy this instance
+            Destroy(gameObject);
+            return;
+        }
+ 
+        // Here we save our singleton instance
+        Instance = this;
+ 
+        // Furthermore we make sure that we don't destroy between scenes (this is optional)
+        DontDestroyOnLoad(gameObject);
+        /////////////////////////////
+
+        channels = new AudioSource[numberOfChannels];
+        for (int i = 0; i < sounds.Length; i++) {
+            if (useFilenameAsSoundName) {
+                soundNames.Add(sounds[i].name);
+                soundMap.Add(sounds[i].name, sounds[i]);
+            } else {
+                soundMap.Add(soundNames[i], sounds[i]);
+            }
+            currentlyPlayingCount.Add(soundNames[i], 0);
+        }
+         
+        // generate attached gameobjects "Channel1", "Channel2" with AudioSources
+        // accessible in the channel array
+        for (int ii = 0; ii < numberOfChannels; ii++) {
+            GameObject c = new GameObject();
+            c.name = "Channel" + ii;
+            c.transform.parent = transform;
+            AudioSource a = c.AddComponent<AudioSource>();
+            a.rolloffMode = AudioRolloffMode.Linear;
+            a.panLevel = 0f;
+            a.dopplerLevel = 0f;
+            c.audio.loop = false;
+            channels[ii] = c.audio;
+        }
+         
+        DontDestroyOnLoad(transform.gameObject);
+ 
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        } else if (cameraFollowMode == CameraFollowMode.Child) {
+            if (Camera.main != null)
+                transform.parent = Camera.main.transform;
+        } 
+     
+        if (initialVolumeFromPreference)
+            volume = GetVolumePreference();
+    }
+ 
+    void OnLevelWasLoaded(int level) {
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        } else if (cameraFollowMode == CameraFollowMode.Child) {
+            if (Camera.main != null)
+                transform.parent = Camera.main.transform;
+        } 
+    }
+ 
+    public void Start() {
+ 
+    }
+ 
+    public void Update() {
+        if (cameraFollowMode == CameraFollowMode.Follow) {
+            if (Camera.main != null)
+                transform.position = Camera.main.transform.position;
+        }
+    }
+ 
+    public float GetVolumePreference() {
+        return PlayerPrefs.GetFloat(preferenceName, volume);
+    }
+ 
+    public void SaveCurrentVolumePreference() {
+        SaveVolumePreference(volume);
+    }
+ 
+    public void SaveVolumePreference(float v) {
+        PlayerPrefs.SetFloat(preferenceName, v);
+        PlayerPrefs.Save();
+    }
+ 
+    public void Play(string soundname) {
+        Play(soundname, volume, 1.0f);
+    }
+ 
+    public void Play(string soundname, float volume, float pitch) {
+        if (!soundMap.ContainsKey(soundname)) {
+            Debug.LogWarning("SoundManager: Tried to play undefined sound: " + soundname);
+            return;
+        }
+        if (soundOn) {
+            if (currentlyPlayingCount[soundname] > maxSimlutSfxOfOneType) {
+                return;
+            }
+            int channelsPlayingCount = 0;
+            while(channels[channelIndex].isPlaying) {
+                incrementChannelIndex();
+                channelsPlayingCount++;
+            }
+            // skip playing this effect if all channels are in use
+            if (channelsPlayingCount >= numberOfChannels) {
+                return;
+            }
+            channels[channelIndex].pitch = pitch;
+            channels[channelIndex].volume = volume;
+			float v = volume;
+			if (ScaleOutputVolume) v = ScaleVolume(volume);
+            channels[channelIndex].PlayOneShot(soundMap[soundname], v);
+            currentlyPlayingCount[soundname] += 1;
+            StartCoroutine("decrementPlayCountInFuture", soundname);
+            incrementChannelIndex();
+        }
+    }
+ 
+    public void Play(string soundname, float volume) {
+        Play(soundname, volume, 1.0f);
+    }
+ 
+	// TODO: we should consider using this dB scale as an option when porting these changes 
+	//       over to unity-bowerbird: http://wiki.unity3d.com/index.php?title=Loudness
+	/*
+	 *   Quadratic scaling of actual volume used by AudioSource. Approximates the proper exponential.
+	 */
+	public float ScaleVolume(float v) {
+		v = Mathf.Pow(v, 4);
+		return Mathf.Clamp01(v);
 	}
-	
-	void OnLevelWasLoaded (int level) {
-		if (cameraFollowMode == CameraFollowMode.Follow) {
-			transform.position = Camera.main.transform.position;
-		}
-		else if (cameraFollowMode == CameraFollowMode.Child) {
-			transform.parent = Camera.main.transform;
-		} 
-	}
-	
-	public void Start () {
-	
-	}
-	
-	public void Update () {
-		if (cameraFollowMode == CameraFollowMode.Follow) {
-			transform.position = Camera.main.transform.position;
-		}
-	}
-	
-	public float GetVolumePreference() {
-		return PlayerPrefs.GetFloat(preferenceName, volume);
-	}
-	
-	public void SaveCurrentVolumePreference() {
-		SaveVolumePreference(volume);
-	}
-	
-	public void SaveVolumePreference(float v) {
-		PlayerPrefs.SetFloat(preferenceName, v);
-		PlayerPrefs.Save();
-	}
-	
-	public void Play(string soundname) {
-		Play(soundname, volume, 1.0f);
-	}
-	
-	public void Play(string soundname, float volume, float pitch) {
-	  if (!soundMap.ContainsKey(soundname)) {
-		  Debug.LogWarning("SoundManager: Tried to play undefined sound: " + soundname);
-		  return;
-	  }
-	  if (soundOn) {
-		if (currentlyPlayingCount[soundname] > maxSimlutSfxOfOneType) {
-			return;
-		}
-		int channelsPlayingCount = 0;
-	    while (channels[channelIndex].isPlaying) {
-	      incrementChannelIndex();
-		  channelsPlayingCount++;
-	    }
-	    // skip playing this effect if all channels are in use
-		if (channelsPlayingCount >= numberOfChannels) {
-			return;
-		}
-	    channels[channelIndex].pitch = pitch;
-	    channels[channelIndex].volume = volume;
-	  	channels[channelIndex].PlayOneShot(soundMap[soundname], volume);
-		currentlyPlayingCount[soundname] += 1;
-		StartCoroutine("decrementPlayCountInFuture", soundname);
-	  	incrementChannelIndex();
-	  }
-	}
-	
-	public void Play(string soundname, float volume) {
-		Play(soundname, volume, 1.0f);
-	}
-	
-	private void incrementChannelIndex() {
-	  	if (channelIndex < channels.Length - 1) {
-	  	   channelIndex++;
-	  	} else {
-	  	   channelIndex = 0;
-	  	}
-	}
-	
-	private IEnumerator decrementPlayCountInFuture(string soundname) {
-		if (Time.timeScale > 0) {
-			yield return new WaitForSeconds(soundMap[soundname].length);
-		}
-		if (currentlyPlayingCount[soundname] > 0) currentlyPlayingCount[soundname] -= 1;
-	}
+
+    private void incrementChannelIndex() {
+        if (channelIndex < channels.Length - 1) {
+            channelIndex++;
+        } else {
+            channelIndex = 0;
+        }
+    }
+ 
+    private IEnumerator decrementPlayCountInFuture(string soundname) {
+        if (Time.timeScale > 0) {
+            yield return new WaitForSeconds(soundMap[soundname].length);
+        }
+        if (currentlyPlayingCount[soundname] > 0)
+            currentlyPlayingCount[soundname] -= 1;
+    }
 }
